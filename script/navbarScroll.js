e979977c8cb4f1edee903acf54332c7a0aab0d70
schahnaz <schahnaz@mail.de>
2018-05-02 15:02:23 +0200

    add shadow to logo when scroll down

--- script/navbarScroll.js
+++ script/navbarScroll.js
@@ -2,11 +2,12 @@ $(document).ready(function() {
 	$(window).scroll(function() {
   	if($(document).scrollTop() > 10) {
     	$('.nav').addClass('change');
+    	$('.nav-tab-big').addClass('change2');
     }
     else {
     $('.nav').removeClass('change');
+	$('.nav-tab-big').removeClass('change2');
+
     }
   });
 });
-
-// addSW
